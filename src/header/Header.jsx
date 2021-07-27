c312dbc29ccf8db8da99395286502b9beef3aaef
noerios <noerios@gmail.com>
2021-07-27 15:01:05 -0500

    video bg added

---
+++ src/header/Header.jsx
@@ -0,0 +1,21 @@
+import "./header.css";
+import React from "react";
+import VideoBg from "reactjs-videobg";
+import herovideo from "../images/HalfwayPoint.mp4";
+import poster from "../images/poster.jpg";
+
+export const Header = () => {
+    return (
+        <div  className="header">
+            <div className="headerTitles">
+                <span className="headerTitleLg">#BYENOE</span>
+                <span className="headerTitleSm">Blog</span>
+            </div>
+            <VideoBg className="headerVid" poster={poster} wrapperClass="videoWrapper">
+                <VideoBg.Source src={herovideo} type="video/mp4" />
+            </VideoBg>
+        </div>
+    )
+}
+
+export default Header
