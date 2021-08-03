09a120669b9e33b31c62b759524358409ffbcb8a
noerios <noerios@gmail.com>
2021-08-03 15:52:11 -0500

    login page added

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./register.css"
+
+const Register = () => {
+    return (
+        <div className="register">
+            <span className="registerTitle">Register</span>
+            <form className="registerForm">
+                <label>Email</label>
+                <input type="text" className="registerInput" placeholder="Enter email..."/>
+                <label>Password</label>
+                <input type="password" className="registerInput" placeholder="Enter password..."/>
+                <button className="registerButton">Register</button>
+            </form>
+            <button className="registerLoginButton">Login</button>
+        </div>
+    );
+}
+
+export default Register;
