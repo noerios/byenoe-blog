2c02bca7e80a16c1215da2e2dba2cef5ab1203e7
noerios <noerios@gmail.com>
2021-08-19 14:11:45 -0500

    social links testing

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -15,15 +15,15 @@ function TopBar() {
     return (
         <div className="top">
             <div className="topLeft">
-            <Link target="_blank" to="https://www.linkedin.com/in/rnoerios/"><i className="topIcon fab fa-linkedin"></i></Link>
-            <Link target="_blank" to="https://github.com/noerios"><i className="topIcon fab fa-github"></i></Link>
-            <Link target="_blank" to="https://www.instagram.com/noway1010/"><i className="topIcon fab fa-instagram"></i></Link>
+            <a target="_blank" to="https://www.linkedin.com/in/rnoerios/"><i className="topIcon fab fa-linkedin"></i></a>
+            <a target="_blank" to="https://github.com/noerios"><i className="topIcon fab fa-github"></i></a>
+            <a target="_blank" to="https://www.instagram.com/noway1010/"><i className="topIcon fab fa-instagram"></i></a>
                 </div>
             <div className="topCenter">
                 <ul className="topList">
                     <li className="topListItem"><Link to="/" className="link">Home</Link></li>
-                    <li className="topListItem"><a target="_blank" to="https://radiant-depths-84649.herokuapp.com//about" className="link">About</a></li>
-                    <li className="topListItem"><a target="_blank" to="https://radiant-depths-84649.herokuapp.com/" className="link">Portfolio</a></li>
+                    <li className="topListItem"><Link target="_blank" to="https://radiant-depths-84649.herokuapp.com//about" className="link">About</Link></li>
+                    <li className="topListItem"><Link target="_blank" to="https://radiant-depths-84649.herokuapp.com/" className="link">Portfolio</Link></li>
                     <li className="topListItem"><Link to="/write" className="link">Write</Link></li>
                     <li className="topListItem" onClick={handleLogout}>{user && "Logout"}</li>
                 </ul>
